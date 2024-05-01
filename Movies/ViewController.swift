7f1cf611f2441bfe7d4fd104e9860acffb04da1a
Harlech1 <turkerkizilcik@gmail.com>
2024-05-01 18:26:41 +0300

    initial commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Türker Kızılcık on 1.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
