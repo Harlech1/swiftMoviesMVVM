536a7cee0441e6762bed277e979d0cf7e0df1163
Harlech1 <turkerkizilcik@gmail.com>
2024-05-23 09:44:50 +0300

    Add: protocols, try again button, alert, fetch func

--- Movies/Controllers/SplashVC.swift
+++ Movies/Controllers/SplashVC.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+protocol DataDelegate: AnyObject{
+    func didFetchMovies(data: Data)
+}
+
 class SplashVC: UIViewController {
+
+    var delegate: DataDelegate?
 
     private let containerView: UIStackView = {
         let view = UIStackView()
@@ -34,27 +40,37 @@ class SplashVC: UIViewController {
         return image
     }()
 
+    lazy var tryAgainButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Try Again", for: .normal)
+        button.backgroundColor = UIColor.systemRed
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(fetchData), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .secondarySystemGroupedBackground
 
+        self.delegate = TabBarController()
+
         addSubviews()
         setupConstraints()
+        fetchData()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            UIApplication.shared.windows.first?.rootViewController = TabBarController()
-        }
     }
 
     private func addSubviews() {
         view.addSubview(containerView)
         containerView.addArrangedSubview(moviesImage)
         containerView.addArrangedSubview(loadingLabel)
+        containerView.addArrangedSubview(tryAgainButton)
     }
 
     private func setupConstraints() {
@@ -63,7 +79,38 @@ class SplashVC: UIViewController {
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
 
             containerView.widthAnchor.constraint(equalToConstant: 100),
-            containerView.heightAnchor.constraint(equalToConstant: 100),
+            containerView.heightAnchor.constraint(equalToConstant: 150),
         ])
+    }
+
+    @objc private func fetchData() {
+        NetworkManager.shared.fetchDataFromAPI { result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    self.delegate?.didFetchMovies(data: data)
+                    self.navigateToTabBar()
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showAlert(message: error.localizedDescription)
+                }
+            }
+        }
+    }
+
+    private func navigateToTabBar() {
+        let tabVC = TabBarController()
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.rootViewController = tabVC
+            window.makeKeyAndVisible()
+        }
+    }
+
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
